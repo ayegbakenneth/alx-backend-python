7cf2c34745ad7d2c08f488694b1ff50a3ef5146f
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-04 14:00:32 +0000

    How to import and use List attribute from typing module.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from typing import List
+""" Importing the attribute list from typing module"""
+def sum_list(input_list: List[float]) -> float:
+    """ Function that recieve a float list and return a float sum"""
+    t_sum = 0.0
+    for value in input_list:
+        t_sum += value
+    return t_sum
