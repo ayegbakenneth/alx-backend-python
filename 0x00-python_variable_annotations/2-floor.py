b66064384475c9dbac085da09549c4d4d17237a7
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-04 16:29:16 +0000

    New update on the use of the math module to implement a floor function

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+""" File executable path """
+
 import math
+""" Introducing the math module """
+
+
 def floor(n: float) -> int:
+    """ A function typed float but return an int value """
     return math.floor(n)
