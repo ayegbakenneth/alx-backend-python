469269ac4989a4a1f526b6b84c7102fb77411155
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-04 09:20:23 +0000

    This code shows us how to use a parameter type annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """ Function that add up only float values"""
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This folder contains code illustrations on Python - Variable Annotations
