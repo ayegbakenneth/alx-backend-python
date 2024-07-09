a48462676ab73828e4bb7b0f8e1f159057f99bfc
ayegbakenneth <ayegbakenneth@gmail.com>
2024-07-09 18:49:28 +0000

    Just testing the code if is working, more update on it.

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" File executable path """
+
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+""" The modules import path """
+
+
+async def async_comprehension():
+    """ This function make use of async_generator
+    function result """
+
+    resultList = [await async_generator()]
+    return resultList
